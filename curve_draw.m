518191f73d8f6d996e0bdf247f1e4c8a4d1de526
Frank <frank770504@gmail.com>
2014-12-30 00:45:37 +0800

    Liniear Seperable Points Sampling

---
+++ curve_draw.m
@@ -0,0 +1,54 @@
+clc
+clear all
+
+global span
+global N
+global Np
+global weight
+
+%=sampling?=%
+is_sample = 1;
+%RandStream.setDefaultStream(RandStream('mt19937ar','seed',0));
+%===========%
+
+%=generate the weighting of f function =%
+N = 2; %Space number
+span = 10; %Range field
+weight = span*(rand(N+1, 1) - 0.5);
+%=======================================%
+
+%=generate the point and label=%
+Np = 200;%point number
+points = span*(rand(Np, N) - 0.5);
+points(:,N+1) = 1;
+label = [];
+for i=1:Np,
+   label = [label; sign(weight'*points(i,:)')];
+end
+points =[points label];
+%==============================%
+
+%=if need sample=%
+if is_sample ==1,
+    Nt = Np;
+    Np = 100;%sample points
+    test_set = points;
+    poinst = [];
+    ind_sample = round(rand(Np,1)*size(test_set,1) + 0.5);
+    points = test_set(ind_sample', :);
+end
+%================&
+
+%=plot the data set=%
+figure
+hold on
+for i=1:Np,
+    if points(i,4)>0,
+        plot(points(i,1), points(i,2),'+');
+    else
+        plot(points(i,1), points(i,2), 'rx');
+    end
+end
+hold off
+%===================%
+axis([-span/2 span/2 -span/2 span/2])
